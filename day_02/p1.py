135878fa875f1ab263e08ac94f035b2c1edce24e
sbreining <sfbreining@gmail.com>
2020-12-02 14:50:07 -0800

    Update Documentation
    Forgot to update the doc-block on the function where the style of counting the required character changed.

--- day_02/p1.py
+++ day_02/p1.py
@@ -5,9 +5,7 @@ def num_of_valid_passwords(pass_list):
     '''
     Password validation requires a range of the required character. The
     parse_string call will return the min and max, as well as the required
-    character and password itself. Iterating through the characters of the
-    password, we'll make sure it hits the expected range. If it is in range,
-    increment valid_passwords.
+    character and password itself. If it is in range, increment valid_passwords.
     '''
     valid_passwords = 0
     for item in pass_list:

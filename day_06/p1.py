5536b0535ac212ed2ea52fd0c306c0e660ee6348
Shane <sfbreining@gmail.com>
2020-12-06 14:24:08 -0800

    Day 6 Clean Up Renaming of variables to make them more meaningful. Adding doc-blocks to the functions explaining what they're doing with the input.

--- day_06/p1.py
+++ day_06/p1.py
@@ -1,17 +1,24 @@
-from string import ascii_lowercase
+from string import ascii_lowercase as questions
 from tools import get_input
 
 
-def day_6():
-    passports = get_input()
+def anyone_answered_yes():
+    '''
+    We received a collection of questionnaires, where the groups of people
+    are separated by a blank line, and an individual's answer is separated
+    by a newline. We iterate through, and find all questions that were
+    answered 'yes' (exist in the questionnaire for the individual) for the
+    whole group. Then, return the sum of questions answered by each group.
+    '''
+    questionnaires = get_input()
 
     count = 0
-    for passport in passports:
-        for c in ascii_lowercase:
-            if c in passport:
+    for questionnaire in questionnaires:
+        for question in questions:
+            if question in questionnaire:
                 count += 1
 
     return count
 
 
-print(day_6())
+print(anyone_answered_yes())
--- day_06/p2.py
+++ day_06/p2.py
@@ -1,22 +1,30 @@
-from string import ascii_lowercase
+from string import ascii_lowercase as questions
 from tools import get_input
 
 
-def day_6():
-    passports = get_input()
+def everyone_answered_yes():
+    '''
+    Very similar to part 1, except here, we want to find all the
+    questions where everyone in a group answered 'yes' (the question
+    is present), instead of just anyone answered yes. Then we sum
+    the questions for which the entire group said yes with the
+    other groups.
+    '''
+    questionnaires = get_input()
 
     count = 0
-    for passport in passports:
-        for c in ascii_lowercase:
-            people = passport.split(' ')
+    for questionnaire in questionnaires:
+        for question in questions:
+            individual_questionnaires = questionnaire.split(' ')
             all_answered = True
-            for person in people:
-                if c not in person:
+            for individual_questionnaire in individual_questionnaires:
+                if question not in individual_questionnaire:
                     all_answered = False
+                    break
             if all_answered:
                 count += 1
 
     return count
 
 
-print(day_6())
+print(everyone_answered_yes())

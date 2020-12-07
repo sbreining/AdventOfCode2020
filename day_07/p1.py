4ac1ca2c636d2d2d9e3fb1001834f94631b6bc35
sbreining <sfbreining@gmail.com>
2020-12-07 15:47:32 -0800

    Day 7 Part 2! (And Cleanup)
    Here lies the solution to part 2, which is how many bags does one shiny gold bag contain?
    Additionally, renamed functions and variables to have meaningful names, and added doc-blocks to the functions describing what they do.
    Honestly, for this day, forget type-hints.

--- day_07/p1.py
+++ day_07/p1.py
@@ -1,7 +1,11 @@
 from tools import get_input
 
 
-def get_colors(bags):
+def parse_bag_rules(bags):
+    '''
+    This will parse the rules of what bags hold other colors.
+    It will return an array of the colors that the bag holds.
+    '''
     if bags == 'no other bags.':
         return []
 
@@ -15,36 +19,60 @@ def get_colors(bags):
     return final_colors
 
 
-def contains_gold(key, value, graph):
-    if 'shiny gold' in value:
-        return True
-    elif not value:
-        return False
-
-    for val in value:
-        if contains_gold(val, graph[val], graph):
-            return True
-    return False
-
-
-def day_07():
+def graph_bags():
+    '''
+    Collects the input, and starts parsing it. First it'll
+    split the string to collect the bag that contains other
+    bags. Then it'll parse the 2nd half of the string to see
+    what bags that bag contained.
+    '''
     rules = get_input()
 
     graph = {}
     for rule in rules:
         bags = rule.split(' bags contain ')
         if graph.get(bags[0]) is not None:
-            graph[bags[0]] += get_colors(bags[1])
+            graph[bags[0]] += parse_bag_rules(bags[1])
         else:
-            graph[bags[0]] = get_colors(bags[1])
+            graph[bags[0]] = parse_bag_rules(bags[1])
+
+        # Make sure there are no duplicates
         graph[bags[0]] = list(dict.fromkeys(graph[bags[0]]))
 
+    return graph
+
+
+def contains_gold(bag_list, graph):
+    '''
+    The bag_list is the list of bags contained within the
+    caller of the function. It'll iterate recursively to
+    see if the top level bag contains a shiney gold bag.
+    '''
+    if 'shiny gold' in bag_list:
+        return True
+    elif not bag_list:
+        return False
+
+    for val in bag_list:
+        if contains_gold(graph[val], graph):
+            return True
+    return False
+
+
+def count_bags_with_gold():
+    '''
+    First, this will call for the graph of bags. Then it'll
+    call the recursive function to find how many top level
+    bags at some point contain a 'shiny gold' bag.
+    '''
+    bag_graph = graph_bags()
+
     count = 0
-    for key, value in graph.items():
-        if contains_gold(key, value, graph):
+    for value in bag_graph.values():
+        if contains_gold(value, bag_graph):
             count += 1
 
     return count
 
 
-print(day_07())
+print(count_bags_with_gold())
--- day_07/p2.py
+++ day_07/p2.py
@@ -1,7 +1,12 @@
 from tools import get_input
 
 
-def get_colors(bags):
+def parse_bag_rules(bags):
+    '''
+    Similar to part 1, this will parse the bag rules, however,
+    the difference here is; this one will keep the number for
+    counting later.
+    '''
     if bags == 'no other bags.':
         return []
 
@@ -15,34 +20,62 @@ def get_colors(bags):
     return final_colors
 
 
-def count_bags(bag, graph):
-    num_col = bag.split(' ')
-    count = int(num_col[0])
-
-    count = count  # TODO Pick it up here.
-
-    return count
-
-
-def day_07():
+def graph_bags():
+    '''
+    Collects the input, and starts parsing it. First it'll
+    split the string to collect the bag that contains other
+    bags. Then it'll parse the 2nd half of the string to see
+    what bags that bag contained.
+    '''
     rules = get_input()
 
     graph = {}
     for rule in rules:
         bags = rule.split(' bags contain ')
         if graph.get(bags[0]) is not None:
-            graph[bags[0]] += get_colors(bags[1])
+            graph[bags[0]] += parse_bag_rules(bags[1])
         else:
-            graph[bags[0]] = get_colors(bags[1])
+            graph[bags[0]] = parse_bag_rules(bags[1])
         graph[bags[0]] = list(dict.fromkeys(graph[bags[0]]))
 
-    print(graph)
-    count = 0
+    return graph
 
-    for bag in graph['shiny gold']:
-        count += count_bags(bag, graph)
+
+def get_number_and_color(contents):
+    '''
+    This definition merely separates the number from the color
+    of the bag.
+    '''
+    temp = contents.split(' ')
+    return (int(temp[0]), temp[1] + ' ' + temp[2])
+
+
+def count_bags_within(bags_within, graph):
+    '''
+    This is quite similar to the first part's recusive counting.
+    Instead, this one recurses for each bag within another bag.
+    There is a way to find the sum of the products, but this recursion
+    was simpler to write in a short timespan.
+    '''
+    count = 0
+    for bag in bags_within:
+        (num_of_bag, bag_color) = get_number_and_color(bag)
+        count += num_of_bag
+
+        for i in range(num_of_bag):
+            count += count_bags_within(graph[bag_color], graph)
 
     return count
 
 
-print(day_07())
+def bags_within_gold():
+    '''
+    This simply graphs the bags, then calls for the count within the
+    'shiny gold' bag. This can really be applied to any key.
+    '''
+    bag_graph = graph_bags()
+
+    return count_bags_within(bag_graph['shiny gold'], bag_graph)
+
+
+print(bags_within_gold())

3f7f6bb4189971b3e28e65ddb2418d913c566669
sbreining <sfbreining@gmail.com>
2020-12-09 12:28:43 -0800

    Day 9 Clean Up
    Renaming of functions and variables to clean up the code.
    Also utilized name == 'main' to reuse part 1 function to find the invalid number.

--- day_09/p1.py
+++ day_09/p1.py
@@ -2,22 +2,34 @@ from tools import get_input
 
 preamble = 25
 
-def day_9():
-    shit = get_input()
+
+def find_invalid_number(values):
+    '''
+    We are informed that any given number has a pair that sums to it
+    in a given subset behind it in the list. So we are lookin for a
+    value that breaks this rule, and return that value.
+    '''
     itr = preamble
-    while itr < len(shit):
+    while itr < len(values):
         i = itr - preamble
-        val_to_check = shit[itr]
+        val_to_check = values[itr]
         has_pair = False
+
         while i < itr:
-            comp = val_to_check - shit[i]
-            if comp in shit[i:itr]:
+            compliment = val_to_check - values[i]
+            if compliment in values[i:itr]:
                 has_pair = True
                 break
-            i+=1
+            i += 1
+
         if not has_pair:
             return val_to_check
+
         itr += 1
+
     return -1
 
-print(day_9())
+
+if __name__ == '__main__':
+    values = get_input()
+    print(find_invalid_number(values))
--- day_09/p2.py
+++ day_09/p2.py
@@ -1,21 +1,51 @@
+from p1 import find_invalid_number
 from tools import get_input
 
-invalid = 675280050
 
-def day_9():
-    shit = get_input()
+def get_min_max(subset: list) -> tuple:
+    '''
+    Simple function to get the minimum and maximum values in the provided
+    list.
+    '''
+    low = subset[0]
+    high = subset[0]
+    for item in subset:
+        if item < low:
+            low = item
+        if item > high:
+            high = item
 
-    contiguous = 2
-    while contiguous < len(shit):
-        itr=0
-        while itr + contiguous < len(shit):
-            cont_nums = shit[itr:itr+contiguous]
-            if sum(cont_nums) == invalid:
-                cont_nums.sort()
-                return cont_nums[0] + cont_nums.pop()
+    return low, high
+
+
+def find_contiguous_subset():
+    '''
+    Given our input for part 1, we found an number that didn't fit the
+    given criteria. We are then told that there exists a contiguous set
+    of numbers in the input that will add to the invalid number. However,
+    we were not told the subset size that will sum to the number. Starting
+    with a set of 2, we'll loop though the input to find a subset of
+    consecutive numbers that sum to the invalid number, incrementing the
+    size of the subset with each loop.
+    '''
+    values = get_input()
+    invalid = find_invalid_number(values)
+
+    subset_length = 2
+    while subset_length <= len(values):
+        itr = 0
+
+        while itr + subset_length <= len(values):
+            contiguous_subset = values[itr:itr+subset_length]
+
+            if sum(contiguous_subset) == invalid:
+                low, high = get_min_max(contiguous_subset)
+                return low + high
+
             itr += 1
-            if itr + contiguous == len(shit):
-                break
-        contiguous+=1
 
-print(day_9())
+        subset_length += 1
+
+
+if __name__ == '__main__':
+    print(find_contiguous_subset())
